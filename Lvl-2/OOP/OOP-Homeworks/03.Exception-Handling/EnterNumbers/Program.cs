d02c944c3a245de63c01704a12047dbdc4dd1a57
Katya Babacheva <kbabacheva@gmail.com>
2014-10-07 22:14:06 +0300

    .

---
+++ Lvl-2/OOP/OOP-Homeworks/03.Exception-Handling/EnterNumbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    class Program
+    {
+      //  int start = 1;
+        const int end = 100;
+
+        public static void Main()
+        {
+            int start = 0;
+            var numbers = new int[10];
+
+            Console.WriteLine("Enter number: [0-100]");
+            for (int i = 1; i < numbers.Length; i++)
+            {
+              //  bool isValid = false;
+                try
+                {
+                    numbers[0] = 0;
+                    numbers[i] = ReadNumber(start, end);
+                    if (numbers[i] < numbers[i-1] )
+                    {
+                        throw new ArithmeticException("Invalid number");
+                    }
+                    Console.WriteLine("Enter number:" + numbers[i] + " < Number < 100");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The number must to be in range [" + numbers[i] + ".." + end + "]");
+                }
+                catch(ArithmeticException)
+                {
+                    Console.WriteLine("The number must be greater than " + numbers[i - 1]);
+                }
+               // Console.WriteLine("Enter number:" + numbers[i] + " < Number < 100");
+            }
+        }
+        public static int ReadNumber(int start, int end)
+        {
+          //  Console.WriteLine("Enter number " + start + " < Number < " + end);
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            if(number > end || number < start)
+            {
+                throw new IndexOutOfRangeException("Number is not in range");
+            }
+            return number;
+        }
+    }
+}
---
+++ Lvl-2/OOP/OOP-Homeworks/03.Exception-Handling/SquareRoot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(input);
+                if (number < 0)
+                {
+                    throw new ArithmeticException("Square root cannot be negative");
+                    
+                }
+                double result = Math.Sqrt(number);
+                Console.WriteLine(result);
+                //Console.WriteLine("Valid operation");
+            }
+            catch (FormatException)
+            {
+                throw new Exception("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("good bye");
+            }
+        }
+    }
+}
